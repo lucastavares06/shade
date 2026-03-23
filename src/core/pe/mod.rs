447f743139cc219b6a62c57375f84da090dcb0f4
lucastavares06 <lucastavareswin10@outlook.com>
2026-03-23 20:30:17 -0300

    feat: export entry point guard from pe module

--- src/core/pe/mod.rs
+++ src/core/pe/mod.rs
@@ -2,4 +2,4 @@ mod exports;
 mod patch;
 
 pub use exports::resolve_remote_export;
-pub use patch::{patch_entry_point, restore_entry_point};
+pub use patch::{patch_entry_point, restore_entry_point, EntryPointGuard};
